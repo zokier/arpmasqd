645db1d47d0edd636f47d4eb4bdc8c0abafc1206
Torste Aikio <zokier@gmail.com>
2015-06-20 20:09:00 +0300

    added code for opening a socket

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
 extern crate libc;
+extern crate errno;
+
+use std::io::Write;
+
+const ETH_P_ARP: libc::c_int = 0x0806;
 
 fn main() {
+    let mut args = std::env::args();
+    if args.len() != 3 {
+        std::io::stderr().write("Usage: arpmasqd LISTEN_ADDR SEND_ADDR\n".as_ref()).unwrap();
+        panic!("number of arguments");
+    }
+    let listen_addr = args.next();
+    let send_addr = args.next();
+    let listen_socket = unsafe { libc::socket(libc::AF_PACKET, libc::SOCK_RAW, ETH_P_ARP.to_be()) };
+    if listen_socket == -1 {
+        let err = errno::errno();
+        panic!("Error opening socket: {} ({})", err, err.0);
+    }
 }
