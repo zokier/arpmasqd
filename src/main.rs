cd759295d39d2ce5584e3960f1aa04f7302c285f
Torste Aikio <zokier@gmail.com>
2015-11-01 18:31:20 +0200

    reorganized code

--- src/main.rs
+++ src/main.rs
@@ -52,7 +52,18 @@ fn ifindex_from_ifname(ifname: &str, sock: libc::c_int) -> libc::c_int {
 
 }
 
-fn main() {
+struct SocketError { 
+    action: &'static str,
+    err: errno::Errno
+}
+
+macro_rules! sockerr {
+    ($action:expr) => {
+        return Err(SocketError{ action: $action, err: errno::errno() });
+    };
+}
+
+fn create_listen_socket(iface: &str) -> Result<libc::c_int, SocketError> {
     // tcpdump -dd arp and incoming
     let bpf_filter_arp_incoming: [sock_filter; 6] = [
         sock_filter { code: 0x28, jt: 0, jf: 0, k: 0x0000000c },
@@ -67,30 +78,19 @@ fn main() {
         filter: bpf_filter_arp_incoming.as_ptr()
     };
 
-    let mut args = std::env::args();
-    if args.len() != 3 {
-        std::io::stderr().write("Usage: arpmasqd LISTEN_IFACE SEND_ADDR\n".as_ref()).unwrap();
-        panic!("number of arguments");
-    }
-    args.next();
-    //unwrap is ok because arg count has been checked
-    let listen_iface = args.next().unwrap();
-    let send_addr = args.next().unwrap();
     let listen_socket = unsafe { libc::socket(libc::AF_PACKET, libc::SOCK_RAW, ETH_P_ALL.to_be() as i32) };
     if listen_socket == -1 {
-        let err = errno::errno();
-        println!("maybe try: sudo setcap CAP_NET_RAW+eip arpmasqd");
-        panic!("Error opening socket: {} ({})", err, err.0);
+        println!("try: sudo setcap CAP_NET_RAW+eip arpmasqd");
+        sockerr!("opening socket");
     }
     let attach_filter_res = unsafe { libc::setsockopt(listen_socket, libc::SOL_SOCKET, SO_ATTACH_FILTER, std::mem::transmute(&bpf_filter_arp_incoming_prog), std::mem::size_of_val(&bpf_filter_arp_incoming_prog) as u32) };
     if attach_filter_res == -1 {
-        let err = errno::errno();
-        panic!("Error attaching filter: {} ({})", err, err.0);
+        sockerr!("attaching filter");
     }
     let listen_sockaddr = libc::sockaddr_ll {
         sll_family: libc::AF_PACKET as u16,
         sll_protocol: ETH_P_ALL.to_be(),
-        sll_ifindex: ifindex_from_ifname(&listen_iface, listen_socket),
+        sll_ifindex: ifindex_from_ifname(&iface, listen_socket),
         sll_hatype: 0,
         sll_pkttype: 0,
         sll_halen: 0,
@@ -98,18 +98,37 @@ fn main() {
     };
     let bind_result = unsafe { libc::bind(listen_socket, std::mem::transmute(&listen_sockaddr), std::mem::size_of_val(&listen_sockaddr) as u32) };
     if bind_result == -1 {
-        let err = errno::errno();
-        panic!("Error binding socket: {} ({})", err, err.0);
+        sockerr!("binding socket");
     }
+    return Ok(listen_socket);
+}
 
+fn do_recv(listen_socket: libc::c_int) -> ! {
     let buf = [0u8; RECV_BUF_LEN];
     let mut recv_sockaddr = unsafe { std::mem::zeroed::<libc::sockaddr_ll>() };
     let mut recv_sockaddr_len: u32 = std::mem::size_of_val(&recv_sockaddr) as u32;
-    let recv_result = unsafe { libc::recvfrom(listen_socket, std::mem::transmute(&buf), RECV_BUF_LEN as u64, 0, std::mem::transmute(&recv_sockaddr), &mut recv_sockaddr_len) };
-    if recv_result == -1 {
-        let err = errno::errno();
-        panic!("Error in recvfrom: {} ({})", err, err.0);
+    loop {
+        let recv_result = unsafe { libc::recvfrom(listen_socket, std::mem::transmute(&buf), RECV_BUF_LEN as u64, 0, std::mem::transmute(&recv_sockaddr), &mut recv_sockaddr_len) };
+        if recv_result == -1 {
+            let err = errno::errno();
+            panic!("Error in recvfrom: {} ({})", err, err.0);
+        }
+        // rust has no offsetof yet so we hardcode 12 here instead
+        println!("From: {}", recv_sockaddr.sll_addr[0..(recv_sockaddr_len as usize -12)].to_hex());
+        println!("Data: {}", buf[0..(recv_result as usize)].to_hex());
     }
-    println!("From: {}", recv_sockaddr.sll_addr[0..6].to_hex());
-    println!("Data: {}", buf[0..(recv_result as usize)].to_hex());
+}
+
+fn main() {
+    let mut args = std::env::args();
+    if args.len() != 2 {
+        std::io::stderr().write("Usage: arpmasqd LISTEN_IFACE\n".as_ref()).unwrap();
+        panic!("number of arguments");
+    }
+    args.next();
+    //unwrap is ok because arg count has been checked
+    let listen_iface = args.next().unwrap();
+    let listen_socket = create_listen_socket(&listen_iface).unwrap_or_else(|err| panic!("Error {}: {} ({})", err.action, err.err, err.err.0));
+
+    do_recv(listen_socket);
 }
